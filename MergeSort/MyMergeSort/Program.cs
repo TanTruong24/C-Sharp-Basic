6afe4b2091cb363a55b0c0c77caca3fb6baff989
Tan The Truong <19522180@gm.uit.edu.vn>
2025-03-19 23:45:28 +0700

    recursion merge sort complete

--- MergeSort/MyMergeSort/Program.cs
+++ MergeSort/MyMergeSort/Program.cs
@@ -9,71 +9,58 @@
 
             List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
 
-            List<int> sorted = new List<int>();
+            List<int> sorted = MergeSort(nums);
 
-            MergeSort(sorted, nums, 0, nums.Count-1);
-
-            Console.WriteLine($"Sorted output: {string.Join(" ", nums)}");
+            Console.WriteLine($"Sorted output: {string.Join(" ", sorted)}");
         }
 
-        static void MergeSort(List<int> sorted, List<int> nums, int startIdx, int endIdx)
+        static List<int> MergeSort(List<int> nums)
         {
-            if (startIdx == endIdx) return;
-            int pivot = (startIdx + endIdx) / 2;
-            MergeSort(sorted, nums, startIdx, pivot);
-            MergeSort(sorted, nums, pivot + 1, endIdx);
-            Merge(sorted, nums, startIdx, pivot, endIdx);
-
-        }
-
-        static void Merge(List<int> sorted, List<int> nums, int startIdx, int pivot, int endIdx)
-        {
-
-            List<int> subLeft = nums.Slice(startIdx, pivot-startIdx + 1);
-            List<int> subRight = nums.Slice(pivot+1, endIdx-(pivot + 1) + 1);
-
-            int storeIdxSubLeft = 0;
-            int storeIdxSubRight = 0;
-            
-            if (subLeft.Count == 1 && subRight.Count == 1)
+            if (nums.Count <= 1)
             {
-                if (subLeft[0] > subRight[0])
-                {
-                    sorted.Add(subRight[0]);
-                    sorted.Add(subLeft[0]);
-                    return;
-                }
-                sorted.Add(subLeft[0]);
-                sorted.Add(subRight[0]);
-                return;
+                return nums;
             }
-            while (storeIdxSubLeft < subLeft.Count || storeIdxSubRight < subRight.Count)
+
+            int mid = nums.Count / 2;
+
+            List<int> leftHalf = MergeSort(nums.GetRange(0, mid));
+            List<int> rightHalf = MergeSort(nums.GetRange(mid, nums.Count-mid));
+
+            List <int> sorted = Merge(leftHalf, rightHalf);
+            return sorted;
+
+        }
+
+        static List<int> Merge(List<int> leftHalf, List<int> rightHalf)
+        {
+            List<int> sorted = new List<int>();
+            int storeIdxLeft = 0;
+            int storeIdxRight = 0;
+
+            while (storeIdxLeft < leftHalf.Count && storeIdxRight < rightHalf.Count) 
             {
-                if (storeIdxSubLeft < subLeft.Count && storeIdxSubRight < subRight.Count)
+                if (leftHalf[storeIdxLeft] >  rightHalf[storeIdxRight])
                 {
-                    if (subLeft[storeIdxSubLeft] < subRight[storeIdxSubRight])
-                    {
-                        sorted.Add((subLeft[storeIdxSubLeft]));
-                        storeIdxSubLeft++;
-                    }
-                    else
-                    {
-                        sorted.Add((subRight[storeIdxSubRight]));
-                        storeIdxSubRight++;
-                    }
-                }
-                else if (storeIdxSubLeft < subLeft.Count)
-                {
-                    sorted.AddRange(subLeft.Slice(storeIdxSubLeft, subLeft.Count - storeIdxSubLeft + 1));
-                    return;
+                    sorted.Add(rightHalf[storeIdxRight]);
+                    storeIdxRight++;
                 }
                 else
                 {
-                    sorted.AddRange(subRight.Slice(storeIdxSubRight, subRight.Count - storeIdxSubRight + 1));
-                    return;
+                    sorted.Add(leftHalf[storeIdxLeft]);
+                    storeIdxLeft++;
                 }
             }
-
+            while (storeIdxLeft < leftHalf.Count)
+            {
+                sorted.Add(leftHalf[storeIdxLeft]);
+                storeIdxLeft++;
+            }
+            while (storeIdxRight < rightHalf.Count)
+            {
+                sorted.Add(rightHalf[storeIdxRight]);
+                storeIdxRight++;
+            }
+            return sorted;
         }
     }
 }
