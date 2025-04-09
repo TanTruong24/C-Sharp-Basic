5e654708f06ddd481e1c5152d61106c95ad8417c
The Tan <truongthetan@v-takeuchi.vn>
2025-04-09 11:52:17 +0700

    ado connect sql server

---
+++ ADOAppSample/ADOAppSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ADOAppSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            TestDatabaseConnection();
+        }
+
+        public static void TestDatabaseConnection()
+        {
+
+            var conn = new SqlConnection("Server=.\\SQLEXPRESS;Database=HR;Trusted_Connection=yes; TrustServerCertificate=True");
+            try
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT employee_id, email FROM employees";
+
+                // sử dụng using tự động đóng, khi thoát khỏi ngoặc {} của using var reader
+                using var reader = cmd.ExecuteReader();
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader.GetInt32(0)}, Name: {reader.GetString(1)}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
---
+++ ADOAppSample/ADOAppSampleTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ADOAppSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOAppSample.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestDatabaseConnectionTest()
+        {
+            Program.TestDatabaseConnection();
+        }
+    }
+}
